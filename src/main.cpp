370d1ca786cbf9636ad71f64e8485d0184af93cc
Simon Lundell <simonlundell@hotmail.com>
2023-09-14 18:59:09 +0200

    memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Needs to be accessed by both CPU and memory
+using Byte = unsigned char;
+using Word = unsigned short;
+
+using u32 = unsigned int;
+
+struct Mem
+{
+	static constexpr u32 MAX_MEM = 1024 * 64;
+	Byte Data[MAX_MEM];
+
+	void Initialise()
+	{
+		for (u32 i = 0; i < MAX_MEM; i++)
+		{
+			Data[i] = 0;
+		}
+	}
+};
+
 struct CPU
 {
-	using Byte = unsigned char;
-	using Word = unsigned short;
 
 	Word PC; // Program counter
 	Word SP; // Stack Pointer
@@ -19,18 +37,21 @@ struct CPU
 	Byte V : 1; // Status flag
 	Byte N : 1; // Status flag
 
-	void Reset()
+	void Reset(Mem& mem)
 	{
 		PC = 0xFFFC;
 		SP = 0x0100;
 		C = Z = I = D = B = V = N = 0;
 		A = X = Y = 0;
+		mem.Initialise();
 	}
 };
 
 int main()
 {
+	Mem mem;
 	CPU cpu;
-	cpu.Reset();
+	// reset also resets the memory
+	cpu.Reset(mem);
 	return 0;
 }
